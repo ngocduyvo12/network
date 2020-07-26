51ef875a498a6393a5061f40ea16e56ea53606a1
Ngoc Vo <ngocduyvo12@yahoo.com>
2020-07-25 22:18:24 -0600

    added form to homepage

--- network/models.py
+++ network/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
+    content = models.TextField()
